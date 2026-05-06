d4f26ed8e411838f87571ad75a81b7e8b03bf41f
choijeongnam <cjn4336@gmail.com>
2026-05-06 16:43:35 +0900

    feat(websocket): move WebSocketConfig Path

--- services/chat/driving/websocket/src/main/java/raio/chat/driving/websocket/config/WebSocketConfig.java
+++ services/chat/driving/websocket/src/main/java/raio/chat/driving/websocket/config/WebSocketConfig.java
@@ -16,9 +16,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("*");
 //                .addInterceptors(stompAuthInterceptor)
-                .withSockJS();
+//                .withSockJS();
     }
 
     @Override
