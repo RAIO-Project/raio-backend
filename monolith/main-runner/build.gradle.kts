cc0f6a8db11ae015cc5c43f2528e997cbd4f8c35
주형 <juhyung3655@hanmail.net>
2026-05-07 23:36:25 +0900

    build: register jwt modules and add dependencies

--- monolith/main-runner/build.gradle.kts
+++ monolith/main-runner/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation(project(":exception-handler-core"))
     implementation(project(":jpa-core"))
     implementation(project(":cors-webmvc"))
+    implementation(project(":jwt-webmvc"))
 
     // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
