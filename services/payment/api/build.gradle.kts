1eda0ac1bf3637d1f770f2acbbc69011a8890d69
silberbullet <pkw961028@naver.com>
2026-06-07 02:32:38 +0900

    feat(payment): add wallet proto

--- services/payment/api/build.gradle.kts
+++ services/payment/api/build.gradle.kts
@@ -1,9 +1,11 @@
 val paymentDomain: String by project
 val paymentException: String by project
 val paymentReadModel: String by project
+val paymentProto: String by project
 
 dependencies {
     api(project(paymentDomain))
     api(project(paymentException))
     api(project(paymentReadModel))
+    api(project(paymentProto))
 }
