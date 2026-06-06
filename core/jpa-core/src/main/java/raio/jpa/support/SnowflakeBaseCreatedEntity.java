d9c72d6ff73b62e0e8f7ad99b01a3adaf4a46327
silberbullet <pkw961028@naver.com>
2026-06-06 16:58:56 +0900

    feat(jpa-core): add **BaseCreatedEntity for append-only table

---
+++ core/jpa-core/src/main/java/raio/jpa/support/SnowflakeBaseCreatedEntity.java
@@ -0,0 +1,22 @@
+package raio.jpa.support;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class SnowflakeBaseCreatedEntity extends SnowflakeBaseEntity {
+    
+    @CreatedDate
+    private Instant createdAt;
+}
--- core/jpa-core/src/main/java/raio/jpa/support/SnowflakeBaseTimeEntity.java
+++ core/jpa-core/src/main/java/raio/jpa/support/SnowflakeBaseTimeEntity.java
@@ -1,13 +1,10 @@
 package raio.jpa.support;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 
@@ -15,10 +12,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class SnowflakeBaseTimeEntity extends SnowflakeBaseEntity {
-    @CreatedDate
-    private Instant createdAt;
+public abstract class SnowflakeBaseTimeEntity extends SnowflakeBaseCreatedEntity {
     
     @LastModifiedDate
     private Instant updatedAt;
