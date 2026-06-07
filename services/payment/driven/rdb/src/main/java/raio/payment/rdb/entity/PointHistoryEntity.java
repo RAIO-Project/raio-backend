1a1a3ec961ba993e7b2924f06abdc57ca2f1f12b
silberbullet <pkw961028@naver.com>
2026-06-07 13:00:57 +0900

    refactor(payment): for append-only table change BaseCreatedEntity

--- services/payment/driven/rdb/src/main/java/raio/payment/rdb/entity/PointHistoryEntity.java
+++ services/payment/driven/rdb/src/main/java/raio/payment/rdb/entity/PointHistoryEntity.java
@@ -5,35 +5,29 @@ import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import raio.jpa.support.LongBaseEntity;
+import raio.jpa.support.LongBaseCreatedEntity;
 import raio.payment.rdb.entity.type.PointHistoryEntityType;
 import raio.payment.rdb.entity.type.PointHistoryEntityTypeConverter;
-
-import java.time.Instant;
 
 @Entity
 @Table(schema = "payment", name = "point_histories")
 @EntityListeners(AuditingEntityListener.class)
-public class PointHistoryEntity extends LongBaseEntity {
+public class PointHistoryEntity extends LongBaseCreatedEntity {
     
     @Column(name = "wallet_id", nullable = false)
     public Long walletId;
-
+    
     @Column(name = "user_id", nullable = false)
     public Long userId;
-
+    
     @Convert(converter = PointHistoryEntityTypeConverter.class)
     @Column(name = "type", nullable = false, length = 20)
     public PointHistoryEntityType type;
-
+    
     @Column(name = "amount", nullable = false)
     public Long amount;
-
+    
     @Column(name = "balance_snapshot", nullable = false)
     public Long balanceSnapshot;
-    
-    @CreatedDate
-    public Instant createdAt;
 }
