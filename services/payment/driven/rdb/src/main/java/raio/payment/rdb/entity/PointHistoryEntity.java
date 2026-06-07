6eae87b2c7c57d5cf964ff12474ef6924250e91a
silberbullet <pkw961028@naver.com>
2026-06-07 13:02:01 +0900

    refactor(payment): for append-only table change BaseCreatedEntity

--- services/payment/driven/rdb/src/main/java/raio/payment/rdb/entity/PointHistoryEntity.java
+++ services/payment/driven/rdb/src/main/java/raio/payment/rdb/entity/PointHistoryEntity.java
@@ -3,16 +3,13 @@ package raio.payment.rdb.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import raio.jpa.support.LongBaseCreatedEntity;
 import raio.payment.rdb.entity.type.PointHistoryEntityType;
 import raio.payment.rdb.entity.type.PointHistoryEntityTypeConverter;
 
 @Entity
 @Table(schema = "payment", name = "point_histories")
-@EntityListeners(AuditingEntityListener.class)
 public class PointHistoryEntity extends LongBaseCreatedEntity {
     
     @Column(name = "wallet_id", nullable = false)
