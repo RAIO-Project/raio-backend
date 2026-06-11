a8ddff0b5ea876f85d58d4bebec5cd2f26063987
silberbullet <pkw961028@naver.com>
2026-06-11 21:21:49 +0900

    feat(batch-runner): add property to raio

--- monolith/batch-runner/build.gradle.kts
+++ monolith/batch-runner/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+val payment: String by project
+
+version = "0.0.1-SNAPSHOT"
+
 dependencies {
     api(project(":batch-core"))
 
@@ -5,4 +11,16 @@ dependencies {
 
     // dbProductFlywayConfig
     runtimeOnly("org.postgresql:postgresql:42.7.4")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<BootJar>{
+    enabled = true
+}
+
+tasks.withType<Jar>{
+    enabled = true
 }
--- monolith/batch-runner/src/main/java/raio/batch/BatchApplication.java
+++ monolith/batch-runner/src/main/java/raio/batch/BatchApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = "club")
-@ConfigurationPropertiesScan(basePackages = "club")
+@SpringBootApplication(scanBasePackages = "raio")
+@ConfigurationPropertiesScan(basePackages = "raio")
 public class BatchApplication {
     
     public static void main(String[] args) {
