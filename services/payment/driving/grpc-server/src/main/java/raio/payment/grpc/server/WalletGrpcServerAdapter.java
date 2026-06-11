e6bbb214a87cc12782820784abd5d6b351e7532d
silberbullet <pkw961028@naver.com>
2026-06-11 20:55:29 +0900

    feat(payment): add donate point service in proto

--- services/payment/driving/grpc-server/src/main/java/raio/payment/grpc/server/WalletGrpcServerAdapter.java
+++ services/payment/driving/grpc-server/src/main/java/raio/payment/grpc/server/WalletGrpcServerAdapter.java
@@ -3,9 +3,12 @@ package raio.payment.grpc.server;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+import raio.payment.application.usecase.PointDonateUseCase;
 import raio.payment.application.usecase.WalletCreateUseCase;
 import raio.payment.grpc.CreateWalletRequest;
 import raio.payment.grpc.CreateWalletResponse;
+import raio.payment.grpc.DonatePointRequest;
+import raio.payment.grpc.DonatePointResponse;
 import raio.payment.grpc.WalletCommandServiceGrpc;
 
 @GrpcService
@@ -14,6 +17,7 @@ public class WalletGrpcServerAdapter
         extends WalletCommandServiceGrpc.WalletCommandServiceImplBase {
     
     private final WalletCreateUseCase walletCreateUseCase;
+    private final PointDonateUseCase pointDonateUseCase;
     
     @Override
     public void createWallet(
@@ -23,6 +27,23 @@ public class WalletGrpcServerAdapter
         var wallet = walletCreateUseCase.create(request.getUserId());
         
         var response = CreateWalletResponse.newBuilder()
+                .setWalletId(wallet.getId())
+                .setUserId(wallet.getUserId())
+                .setBalance(wallet.getBalance())
+                .build();
+        
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+    
+    @Override
+    public void donatePoint(
+            DonatePointRequest request,
+            StreamObserver<DonatePointResponse> responseObserver
+    ) {
+        var wallet = pointDonateUseCase.donate(request.getWalletId(), request.getAmount());
+        
+        var response = DonatePointResponse.newBuilder()
                 .setWalletId(wallet.getId())
                 .setUserId(wallet.getUserId())
                 .setBalance(wallet.getBalance())
